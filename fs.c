8788857533b26b233d20a20170844f63e0b39dd8
haksang <haksang0928@gmail.com>
2015-11-30 16:37:20 +0900

    layout

--- fs.c
+++ fs.c
@@ -9,7 +9,7 @@
 #include <sys/time.h>
 #include <unistd.h>
 
-struct _inode {
+typedef struct _inode {
 		char name[14];
 		unsigned char data[128];
 		struct stat st;
@@ -18,13 +18,13 @@ struct _inode {
 		struct	inode	*child;
 		struct	inode	*next;
 		struct	inode	*before;
-};
+} inode;
 
-static _inode root_node;
+static inode* root_node;
 
 static void *hello_init(struct fuse_conn_info *conn)
 {
-	puts("Hello_init!!");
+	printf("Hello_init");
 
 	(void) conn;
 
@@ -36,28 +36,30 @@ static void *hello_init(struct fuse_conn_info *conn)
 	root_node->next = NULL;
 	root_node->before = NULL;
 
-	strcpy(root_node.name, "/");
+	strcpy(root_node->name, "/");
 
-	root_node.st.st_mode = (S_IFDIR | 0755);
-	root_node.st.st_nlink = 2;
-	root_node.st.st_mtine = current_time;
-	root_node.st.st_ctime = current_time;
-	root_node.st.st_atime = current_time;
-	
+	root_node->st.st_mode = (S_IFDIR | 0755);
+	root_node->st.st_nlink = 2;
+	root_node->st.st_mtime = current_time;
+	root_node->st.st_ctime = current_time;
+	root_node->st.st_atime = current_time
+
+	printf("Hello_init end")
+
 	return NULL;
 }
 static int hello_getattr(const char *path, struct stat *stbuf)
 {
-	puts("Hello_Getattr");
+	printf("Hello_Getattr");
 	inode *node;
 	node = root_node;
 
 	if ( node == NULL) {
-		puts("file not exist");
+		printf("file not exist");
 		return -ENOENT;
 	}
 	else {
-		puts("file exist");
+		printf("file exist");
 		memset(stbuf, 0, sizeof(struct stat));
 		stbuf->st_mode = node->st.st_mode; /* protection */
 		stbuf->st_nlink = node->st.st_nlink; /* number of hard links */
@@ -66,12 +68,14 @@ static int hello_getattr(const char *path, struct stat *stbuf)
 		stbuf->st_ctime = node->st.st_ctime; /* time of last status change */
 		stbuf->st_mtime = node->st.st_mtime; /* time of last modificaiton */
 	}
+	printf("Hello_Getattr end");
 	return 0;
 }
 
 static int hello_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
 			 off_t offset, struct fuse_file_info *fi)
 {
+	printf("Hello_readdir");
 	(void) offset;
 	(void) fi;
 	inode *node;
@@ -83,49 +87,57 @@ static int hello_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
 
 	filler(buf, ".", NULL, 0);
 	filler(buf, "..", NULL, 0);
+	filler(buf,node->name, NULL, 0);
 
-	puts("Hello_readdir");
+	printf("Hello_readdir end");
   return 0;
 }
 
 static int hello_open(const char *path, struct fuse_file_info *fi)
 {
-	puts("hello_open");
+	printf("hello_open");
+	printf("hello_open end");
 	return 0;
 }
 
 static int hello_read(const char *path, char *buf, size_t size, off_t offset,
 		      struct fuse_file_info *fi)
 {
-	puts("hello_read");
+	printf("hello_read");
+	printf("hello_read end");
   return size;
 }
 
 static int hello_write(const char *path, const char *buf, size_t size,
 			off_t offset, struct fuse_file_info *fi)
 {
-	 puts("hello_write");
+	 printf("hello_write");
+	 printf("hello_write end");
 	 return size;
 }
 
 static int hello_mkdir(const char *path, mode_t mode)
 {
- 	puts("hello_mkdir");
+ 	printf("hello_mkdir");
+	printf("hello_mkdir end");
 }
 
 static int hello_rmdir(const char *path)
 {
- 	puts("hello_rmdir");
+ 	printf("hello_rmdir");
+	printf("hello_rmdir end");
 }
 
 static int hello_mknod(const char *path, mode_t mode)
 {
- 	puts("hello_mknod");
+ 	printf("hello_mknod");
+	printf("hello_mknod end");
 }
 
 static int hello_unlink(const char *path)
 {
- 	puts("hello_unlink");
+ 	printf("hello_unlink");
+	printf("hello_unlink end");
 }
 static struct fuse_operations hello_oper = {
 		.init		= hello_init,
@@ -137,7 +149,9 @@ static struct fuse_operations hello_oper = {
 		.mkdir		= hello_mkdir,
 		.rmdir		= hello_rmdir,
 		.mknod		= hello_mknod,
-		.unlink		= hello_unlink
+		.unlink		= hello_unlink,
+		.truncate = hello_truncate,
+		.utimens  = hello_utimens
 };
 
 int main(int argc, char *argv[])
